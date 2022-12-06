68f5a242f3ed662c22cc8944f8ead4c4ef461f88
Barrett Smith <barrett@datasmith.net>
2022-12-06 10:54:07 -0500

    Completed Day 6 solution.

--- day6.py
+++ day6.py
@@ -10,10 +10,28 @@ Test data
     zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
 
 Part 1: How many characters need to be processed before the first start-of-packet marker is detected?
+
+Part 2: Now with 14 characters, where does the first message marker end?
 """
+
+
+def findFirstMarker(datastream: str, markerLength=4):
+    for i in range(markerLength, len(datastream)):
+        rng = datastream[i - markerLength:i]
+        uniq = set(rng)
+        if len(rng) == len(uniq):
+            return i, rng
+
 
 if __name__ == '__main__':
     # @todo: add error handling
     inFile = open('./inputData/day6', 'r')
-    data = [list(line.strip()) for line in inFile]
+    data = inFile.read()
     inFile.close()
+
+    i, seq = findFirstMarker(data)
+    print(f'Part 1: The first marker is {seq}, ending at character {i}.')
+
+    i, seq = findFirstMarker(data, 14)
+    print(f'Part 2: The first marker is {seq}, ending at character {i}.')
+
