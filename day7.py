e9e1e2c15f65618b74d2ce050da09471dc8dbf74
Barrett Smith <barrett@datasmith.net>
2022-12-07 06:43:42 -0500

    Initial commit of day 7 data file and stub script.

---
+++ day7.py
@@ -0,0 +1,8 @@
+"""
+To begin, find all of the directories with a total size of at most 100000, then calculate the sum of their total sizes.
+In the example above, these directories are a and e; the sum of their total sizes is 95437 (94853 + 584). (As in this example, this process can count files more than once!)
+
+Part 1: Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those
+ directories?
+
+"""
