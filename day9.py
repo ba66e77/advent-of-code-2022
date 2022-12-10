0f4f1fe2a408f71d95c45f6933daeff37ea7da04
Barrett Smith <barrett@datasmith.net>
2022-12-10 18:06:35 -0500

    Completing Day 9 part 2 and replacing original part 1 file with new complete Day 9 file.

--- day9.py
+++ day9.py
@@ -8,29 +8,44 @@ rules
 2. Otherwise, if the head and tail aren't touching and aren't in the same row or column, the tail always moves one step
     diagonally to keep up
 
-
 Part 1: Simulate your complete hypothetical series of motions. How many positions does the tail of the rope visit at
 least once?
 
+Part 2: Simulate your complete series of motions on a larger rope with ten knots. How many positions does the tail of
+the rope visit at least once?
+
 """
-
-
 def part1(data):
 
-    rope = Rope()
+    knots = 2
+    rope = Rope(knots)
 
     for d in data:
         direction, steps = d.split()
         rope.moveHead(direction, int(steps))
 
-    print(f"Tail visited {len(set(rope.getTailPositionHistory()))} positions.")
-    print(rope.getTailPositionHistory())
+    print(f"Part 1: With {knots} knots, tail visited {len(set(rope.getTailPositionHistory()))} positions.")
+
+def part2(data, knots: int):
+
+    rope = Rope(knots)
+
+    for d in data:
+        direction, steps = d.split()
+        rope.moveHead(direction, int(steps))
+
+    print(f"Part 2: With {knots} knots, tail visited {len(set(rope.getTailPositionHistory()))} positions.")
 
 
 class Rope:
 
-    def __init__(self):
-        self.s = self.headPosition = self.tailPosition = (0, 0)
+    def __init__(self, knots: int):
+        self.knotPositions = []
+
+        # initialize knot positions
+        for i in range(0, knots):
+            self.knotPositions.append((0, 0))
+
         self.tailPositionHistory = []
         self._recordTailPositionHistory()
 
@@ -41,31 +56,53 @@ class Rope:
     def getTailPositionHistory(self):
         return self.tailPositionHistory
 
+    def _getHeadPosition(self):
+        return self.knotPositions[0]
+
+    def _setHeadPosition(self, newPosition: tuple[int]):
+        self._setPosition(0, newPosition)
+
+    def _setPosition(self, index: int, newPosition: tuple[int]):
+        self.knotPositions[index] = newPosition
+
     def _moveHeadOnePosition(self, direction: str):
-        oldX, oldY = self.headPosition
+        oldX, oldY = self._getHeadPosition()
 
         match direction:
             # move on Y axis
             case 'U':
-                self.headPosition = (oldX, oldY+1)
+                self._setHeadPosition((oldX, oldY+1))
             case 'D':
-                self.headPosition = (oldX, oldY-1)
+                self._setHeadPosition((oldX, oldY-1))
 
             # move on X axis
             case 'L':
-                self.headPosition = (oldX-1, oldY)
+                self._setHeadPosition((oldX-1, oldY))
             case 'R':
-                self.headPosition = (oldX+1, oldY)
+                self._setHeadPosition((oldX+1, oldY))
 
             # Dafuq did you do??
             case _:
                 raise Exception(f"Unexpected direction: {direction}")
 
-        self._moveTailOnePosition()
+        self._moveTailsOnePosition(self._getHeadPosition())
 
-    def _moveTailOnePosition(self):
+    def _moveTailsOnePosition(self, headPosition):
+
+        for i in range(1,len(self.knotPositions)):
+            newPosition = self._moveTailOnePosition(self.knotPositions[i-1], self.knotPositions[i])
+
+            if newPosition is None:
+                break
+            else:
+                self.knotPositions[i] = newPosition
+
+        self._recordTailPositionHistory()
+
+    def _moveTailOnePosition(self, leaderPosition, followerPosition) -> tuple:
+
         # check if move needed
-        variance = (self.headPosition[0] - self.tailPosition[0], self.headPosition[1] - self.tailPosition[1])
+        variance = (leaderPosition[0] - followerPosition[0], leaderPosition[1] - followerPosition[1])
 
         if abs(variance[0]) <= 1 and abs(variance[1]) <= 1:
             # no move needed
@@ -77,21 +114,19 @@ class Rope:
             ):
             # move diagonally
             newPosition = (
-                self._moveDiagonally(self.tailPosition[0], variance[0]),
-                self._moveDiagonally(self.tailPosition[1], variance[1])
+                self._moveDiagonally(followerPosition[0], variance[0]),
+                self._moveDiagonally(followerPosition[1], variance[1])
             )
         else:
             # figure out which way to move
             newPosition = (
-                self._getNewTailCoordinate(self.tailPosition[0], variance[0]),
-                self._getNewTailCoordinate(self.tailPosition[1], variance[1])
+                self._getNewTailCoordinate(followerPosition[0], variance[0]),
+                self._getNewTailCoordinate(followerPosition[1], variance[1])
             )
 
         # do that shizzit
-        self.tailPosition = newPosition
+        return newPosition
 
-        # record position
-        self._recordTailPositionHistory()
 
     def _getNewTailCoordinate(self, coordinatePosition: int, coordinateVariance: int) -> int:
 
@@ -117,10 +152,11 @@ class Rope:
 
         return coordinatePosition
 
-
-
     def _recordTailPositionHistory(self):
-        self.tailPositionHistory.append(self.tailPosition)
+        self.tailPositionHistory.append(self._getTailPosition())
+
+    def _getTailPosition(self):
+        return self.knotPositions[-1]
 
 
 if __name__ == '__main__':
@@ -138,5 +174,5 @@ if __name__ == '__main__':
     data = inFile.readlines()
     inFile.close()
 
-    print(len(data))
     part1(data)
+    #part2(data, 10)
